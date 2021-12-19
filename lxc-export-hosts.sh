d9fa5b78cffc489f3924689cb4dc853d2d69e1f1
Takács Ákos <rimelek@rimelek.hu>
2021-12-19 16:45:48 +0100

    fix know_hosts permission

--- lxc-export-hosts.sh
+++ lxc-export-hosts.sh
@@ -14,6 +14,7 @@ output="$(cat "$inputfile")"
 
 while IFS="" read -r line || [ -n "$line" ]; do
   ip="$(echo "$line" | cut -d " " -f1)"
+
   host="$(echo "$line" | cut -d " " -f2-)"
 
   ip_regex="${ip//./\\.}"
@@ -21,9 +22,11 @@ while IFS="" read -r line || [ -n "$line" ]; do
 
   output="$(echo "$output" | sed "/^\($ip_regex\) .*/d")"
   output="$(echo "$output" | sed "s/^\([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\( .\+\)\?\)\( $host_regex\)\( \|$\|#\)\(.*\)/\1\3.removed\4\5/g")"
-  output="$(echo "$output" | sed "$ a$ip $host # lxc automatic hostmap")"
-
+  
+  if [[ -n "$ip" ]]; then
+    output="$(echo "$output" | sed "$ a$ip $host # lxc automatic hostmap")"
+  fi
   ssh-keygen -f ~/.ssh/known_hosts -R "$host" &>/dev/null || true
 done < <(echo "$hostmap")
 
-echo "$output" > "$outputfile"
+echo "$output" | sudo tee "$outputfile" > /dev/null
